c59da032cc6c07dbedc7a8810b14ba00ea608951
danielharvell-work <danielharvell.work@gmail.com>
2026-02-13 14:56:39 -0700

    Initial commit: Global water level flooding visualization with CesiumJS

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting local server for Flooding visualization...
+echo.
+echo Open your browser to: http://localhost:8000/flooding.html
+echo.
+echo Press Ctrl+C to stop the server.
+echo.
+python -m http.server 8000
+
